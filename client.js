be93e5061fc82543f627ef5dac3462efa523cf3e
Grimka <julien.decroix64@gmail.com>
2020-07-10 15:10:03 +0200

    socket dont auto reconnect

--- client.js
+++ client.js
@@ -1,24 +1,23 @@
-import {decode} from '@msgpack/msgpack';
-import {encode} from '@msgpack/msgpack';
-import SimpleSocket from './SimpleSocket';
+import { decode } from "@msgpack/msgpack";
+import { encode } from "@msgpack/msgpack";
+import SimpleSocket from "./SimpleSocket";
 
-function DESocket( url, options ) 
-{
-  SimpleSocket.call( this );
+function DESocket(url, options) {
+  SimpleSocket.call(this);
 
   this.id = null; // automatically pushed by server
   this.options = {
     pingInterval: 5000,
-    debug: false
+    debug: false,
   };
-  
+
   options = options || {};
-  for ( var i in options ) {
-    this.options[ i ] = options[ i ];
+  for (var i in options) {
+    this.options[i] = options[i];
   }
   this.url = url;
 
-  this.listen( 'id', id => this.id = id );
+  this.listen("id", (id) => (this.id = id));
   return this;
 }
 
@@ -27,63 +26,66 @@ DESocket.constructor = DESocket;
 DESocket.supr = SimpleSocket.prototype;
 
 // to override
-DESocket.prototype.onOpen = function(){};
-DESocket.prototype.onMessage = function(){};
-DESocket.prototype.onClose = function(){};
+DESocket.prototype.onOpen = function () {};
+DESocket.prototype.onMessage = function () {};
+DESocket.prototype.onClose = function () {};
 
-DESocket.prototype.connect = function( url ) {
+DESocket.prototype.connect = function (url) {
   url = url || this.url;
 
-  this._ws = new WebSocket( url );
+  this._ws = new WebSocket(url);
   this.url = url;
   this.id = null;
   this._open = false;
 
   this._ws.onopen = () => this._onOpen();
-  this._ws.onmessage = ( msg ) => this._onMessage( msg );
+  this._ws.onmessage = (msg) => this._onMessage(msg);
   this._ws.onclose = (event) => this._onClose(event);
 };
 
-DESocket.prototype.keepAlive = function() {
-  if ( this._ws.readyState !== this._ws.OPEN ) {
+DESocket.prototype.keepAlive = function () {
+  if (this._ws.readyState !== this._ws.OPEN) {
     return;
   }
-  this._ws.send( encode( { _:'1'} ) ); // ping
-}
+  this._ws.send(encode({ _: "1" })); // ping
+};
 
-DESocket.prototype._onOpen = function() {
-  if ( this.options.debug ) {
-    console.log( 'socket connected' );
+DESocket.prototype._onOpen = function () {
+  if (this.options.debug) {
+    console.log("socket connected");
   }
 
-  clearInterval( this.pingInterval );
-  this.pingInterval = setInterval( () => this.keepAlive(), this.options.pingInterval );
-  
-  if(!this._open) {
+  clearInterval(this.pingInterval);
+  this.pingInterval = setInterval(
+    () => this.keepAlive(),
+    this.options.pingInterval
+  );
+
+  if (!this._open) {
     this._open = true;
     this.onOpen();
   }
 };
 
-DESocket.prototype._onMessage = function( msg ) {
+DESocket.prototype._onMessage = function (msg) {
   var reader = new FileReader();
-  reader.addEventListener( 'loadend', () => {
+  reader.addEventListener("loadend", () => {
     var obj = decode(reader.result);
 
     //console.log(JSON.stringify(obj));
 
-    if ( this._events[ obj._ ] ) {
-      this._events[ obj._ ].apply( this, obj.d );
+    if (this._events[obj._]) {
+      this._events[obj._].apply(this, obj.d);
     }
 
-    this.onMessage( obj );
-  } );
-  reader.readAsArrayBuffer( msg.data );
+    this.onMessage(obj);
+  });
+  reader.readAsArrayBuffer(msg.data);
 };
 
-DESocket.prototype._onClose = function(event) {
-  if ( this.options.debug ) {
-    console.log('socket disconnected', arguments);
+DESocket.prototype._onClose = function (event) {
+  if (this.options.debug) {
+    console.log("socket disconnected", arguments);
   }
 
   if (this._open) {
@@ -92,9 +94,9 @@ DESocket.prototype._onClose = function(event) {
   }
 
   // if the socket is not tagged as "_manualClose", it will try to reconnect indefinitively
-  if ( !this._manualClose ) {
-    setTimeout( () => this.connect(), 1000 );
-  }
+  /*if (!this._manualClose) {
+    setTimeout(() => this.connect(), 1000);
+  }*/
 };
 
 // only one way to disconnect on front
